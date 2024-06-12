530e3a2a2a34e3f2d17663096dc743ade3c2ea9e
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-06-12 23:16:05 +0300

    add landing link

--- src/components/user/SignIn.jsx
+++ src/components/user/SignIn.jsx
@@ -1,23 +1,53 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 "use client";
+import { useState } from "react";
 import { cn } from "../../../utils/cn";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-} from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
 import { NavLink } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import { loginUser } from "../../hooks/api";
+import { useAuth } from "../../context/auth";
 export function SignIn() {
-  const handleSubmit = (e) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loginError, setLoginError] = useState(null);
+  const [status, setStatus] = useState("idle");
+
+  const { setIsLoggedIn, setUserInfo, setToken } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.state?.pathname || "/";
+
+  const handleChange = (e) => {
+    setLoginError(null);
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [id]: value }));
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form submitted");
+    setStatus("submitting");
+    try {
+      const loginData = await loginUser(formData);
+      setIsLoggedIn(true);
+      setStatus("idle");
+      setUserInfo(loginData);
+      setToken(loginData.access_token);
+      navigate(pathname, { replace: true });
+    } catch (err) {
+      setLoginError(err.message);
+      console.log(err.message);
+      setStatus("idle");
+    }
   };
 
   return (
-    <div className="w-full max-w-md p-4 mx-auto bg-white rounded-none shadow-lg md:rounded-2xl md:p-8 dark:bg-black shadow-slate-500">
+    <div className="w-full max-w-md p-4 mx-auto bg-white rounded-none shadow-lg md:rounded-2xl md:p-8 dark:bg-gray-800 shadow-slate-500">
       <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
         Welcome to Jobly
       </h2>
@@ -28,34 +58,44 @@ export function SignIn() {
       <form className="my-8" onSubmit={handleSubmit}>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <Input
+            id="email"
+            placeholder="projectmayhem@fc.com"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input
+            id="password"
+            placeholder="••••••••"
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </LabelInputContainer>
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          disabled={status === "submitting"}
           type="submit"
         >
-          Sign In &rarr;
+          {status === "submitting" ? "Logging in..." : "Log in"} &rarr;
           <BottomGradient />
         </button>
 
-        <div className="pb-0 mt-6">
-            <p className="text-lg">
-            Don't have an account? 
-            <NavLink to='/sign-up' className='underline text-primary'> Sign Up</NavLink>
-            </p>
-        </div>
+        {loginError && <p className="error m-3 text-red-600 text-center">{loginError}</p>}
+
+        <p className="text-center mt-4 dark:text-white">or sign in with</p>
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-5 h-[1px] w-full" />
 
         <div className="flex flex-col space-y-4">
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
+            type="button"
           >
             <IconBrandGithub className="w-4 h-4 text-primary dark:text-neutral-300" />
             <span className="text-sm text-neutral-700 dark:text-neutral-300">
@@ -65,7 +105,7 @@ export function SignIn() {
           </button>
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
+            type="button"
           >
             <IconBrandGoogle className="w-4 h-4 text-primary dark:text-neutral-300" />
             <span className="text-sm text-neutral-700 dark:text-neutral-300">
@@ -73,6 +113,15 @@ export function SignIn() {
             </span>
             <BottomGradient />
           </button>
+        </div>
+        <div className="pb-0 mt-6">
+          <p className="text-lg dark:text-white">
+            Don't have an account?
+            <NavLink to="/landing-page" className="underline text-primary">
+              {" "}
+              Sign Up
+            </NavLink>
+          </p>
         </div>
       </form>
     </div>
