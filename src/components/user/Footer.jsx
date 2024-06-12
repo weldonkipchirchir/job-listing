1643b40b825fd36f85c64a1f4af6900961118cc4
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-06-12 23:07:59 +0300

    feat: Dynamic link selection in Footer based on account type

--- src/components/user/Footer.jsx
+++ src/components/user/Footer.jsx
@@ -1,12 +1,24 @@
 import { NavLink } from "react-router-dom";
-import { links } from "../../utils/links";
-import { getActiveStyles } from "../../utils/links";
 import { CiTwitter } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
 import { AiOutlineMail } from "react-icons/ai";
-
+import { guestLinks, userLinks, adminLinks, getActiveStyles } from "../../utils/links";
 function Footer() {
   const year = new Date().getFullYear();
+
+  const loggedIn = true;
+  // const account = "";
+  const account = "user";
+
+  let links;
+
+  if (loggedIn && account == "user") {
+    links = userLinks;
+  } else if (loggedIn && account == "admin") {
+    links = adminLinks;
+  } else {
+    links = guestLinks;
+  }
 
   return (
     <div className=" padding border-t-2 border-gray-400">
@@ -17,7 +29,7 @@ function Footer() {
               key={index}
               to={link.link}
               style={({ isActive }) => getActiveStyles(isActive, link.name)}
-              className="border-b-2 hover:border-primary text-center"
+              className="border-b-2 dark:text-white hover:border-primary text-center"
             >
               {link.name}
             </NavLink>
@@ -25,12 +37,12 @@ function Footer() {
         </ul>
       </div>
       <div className="flex justify-center gap-5 py-8">
-        <CiTwitter className="w-9 h-9" />
-        <CiLinkedin className="w-9 h-9" />
-        <AiOutlineMail className="w-9 h-9" />
+        <CiTwitter className="w-9 h-9 dark:text-white" />
+        <CiLinkedin className="w-9 h-9 dark:text-white" />
+        <AiOutlineMail className="w-9 h-9 dark:text-white" />
       </div>
       <div className="flex justify-center">
-        <p className="text-xl">@{year} Jobly Inc.</p>
+        <p className="text-xl dark:text-white">@{year} Jobly Inc.</p>
       </div>
     </div>
   );
