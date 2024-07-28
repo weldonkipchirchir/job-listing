68115f968edda7a8143ba160c466ab79b8219aa6
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-07-28 17:34:14 +0300

    add options

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-const ButtonGroup = ({ options, selectedOption, onChange }) => {
+const ButtonGroup = ({ options, type, onChange }) => {
     
   return (
     <div className="flex flex-wrap justify-start gap-3 items-center my-2">
@@ -8,7 +8,7 @@ const ButtonGroup = ({ options, selectedOption, onChange }) => {
         <button
           type="button"
           key={option}
-          className={`px-4 py-1 text-[16px] font-bold ${selectedOption === option ? 'bg-primary text-white' : 'bg-gray-200 text-gray-700'} rounded`}
+          className={`px-4 py-2 rounded ${type === option ? 'bg-primary text-white' : 'bg-gray-200 text-gray-700'}`}
           onClick={() => onChange(option)}
         >
           {option}
