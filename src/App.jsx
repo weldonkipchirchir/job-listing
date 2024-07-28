224c6749419e631c23c897f9285871195900ede4
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-07-28 17:38:47 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,67 @@ const NotFound = lazy(() => import("./components/NotFound"));
 const Home = lazy(() => import("./pages/user/Home"));
 import SignUp from "./pages/user/SignUp";
 import SignIn from "./pages/user/SIgnIn";
-import JobListing from "./pages/user/JobListing";
-import JobDetails from "./pages/user/JobDetails";
+import JobListing, {
+  loader as JobListingLoader,
+} from "./pages/user/JobListing";
+import JobDetails, {
+  loader as JobDetailsLoader,
+} from "./pages/user/JobDetails";
 import JobApply from "./pages/user/JobApply";
 import FindJob from "./pages/user/FindJob";
 import SavedJob from "./pages/user/SavedJobs";
 import AboutUs from "./pages/user/AboutUs";
 import Dashboard from "./pages/admin/Dashboard";
-import JobListingAdmin from "./pages/admin/JobListing";
+import JobListingAdmin, {loader as AdminListingLoader} from "./pages/admin/JobListing";
 import AddJob from "./pages/admin/AddJob";
+import Settings from "./pages/admin/Settings";
+import ApplicationsPage, {loader as adminApplicationsLoader} from "./pages/admin/Applications";
+import ApplicationDetailsPage from "./pages/admin/ApplicationDetails";
+import ApplicationDetails from "./pages/user/JobApplicationsDetails";
+import UserSettings from "./pages/user/UserSettings";
+import LandingPage from "./pages/user/LandingPage";
+import EditJob, {loader as EditLoader} from "./pages/admin/EditJob";
+import { ProtectedRoute } from "./components/Protected";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />} errorElement={<Error />}>
       <Route index element={<Home />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/job-listing" element={<JobListing />} />
-      <Route path="/job-details" element={<JobDetails />} />
-      <Route path="/job-apply" element={<JobApply />} />
-      <Route path="/find-job" element={<FindJob />} />
-      <Route path="/saved-jobs" element={<SavedJob />} />
+      <Route path="/landing-page" element={<LandingPage />} />
       <Route path="/about-us" element={<AboutUs />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/jobs-listing" element={<JobListingAdmin />} />
-      <Route path="/add-job" element={<AddJob />} />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/job-listing"
+          element={<JobListing />}
+          loader={JobListingLoader}
+        />
+        <Route path="/job-details">
+          <Route
+            path=":id"
+            element={<JobDetails />}
+            loader={JobDetailsLoader}
+          />
+        </Route>
+        <Route path="job-apply" element={<JobApply />} />
+        <Route path="/find-job" element={<FindJob />} />
+        <Route path="/bookmarks" element={<SavedJob />} />
+        <Route path="/user-settings" element={<UserSettings />} />
+        <Route path="applications-info" element={<ApplicationDetails />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jobs-listing" element={<JobListingAdmin />} loader={AdminListingLoader} />
+        <Route path="/add-job" element={<AddJob />} />
+        <Route path="/edit-job/:id" element={<EditJob />} loader={EditLoader}/>
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/applications">
+          <Route index element={<ApplicationsPage />} loader={adminApplicationsLoader}/>
+          <Route
+            path="application-details/:id"
+            element={<ApplicationDetailsPage />}
+          />
+        </Route>
+      </Route>
+
       <Route path="*" element={<NotFound />} />
     </Route>
   )
@@ -43,7 +80,7 @@ const router = createBrowserRouter(
 
 function App() {
   return (
-    <div>
+    <div className="dark:bg-gray-900">
       <Suspense
         fallback={
           <div className="flex items-center justify-center">
